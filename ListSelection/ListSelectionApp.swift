3a90e9aa584b35cc079d1a7ca1287bef012b7fcd
Szabolcs Tóth <tsz@purzelbaum.hu>
2021-11-07 17:44:27 +0100

    Initial Commit

---
+++ ListSelection/ListSelectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListSelectionApp.swift
+//  ListSelection
+//
+//  Created by Szabolcs Tóth on 2021. 11. 07..
+//
+
+import SwiftUI
+
+@main
+struct ListSelectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
